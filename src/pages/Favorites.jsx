10f7b894d443a33689fb1289bf06a99c2729da30
Hebertt Nascimento <heberttaferreira.com@gmail.com>
2022-10-03 17:20:31 -0300

    first req

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+
+class Favorite extends Component {
+  render() { return (<div data-testid="page-favorites" />); }
+}
+export default Favorite;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div data-testid="page-not-found" />
+    );
+  }
+}
+export default NotFound;
