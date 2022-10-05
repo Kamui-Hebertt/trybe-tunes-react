9ce222d72a56fd8f0c6fd4230226faebdf39ebd8
Hebertt Nascimento <heberttaferreira.com@gmail.com>
2022-10-05 20:05:12 -0300

    req 7 done

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,17 +1,80 @@
 import React, { Component } from 'react';
+import * as PropTypes from 'prop-types';
 import Header from './compoents/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from './compoents/MusicCard';
+import Loading from './compoents/Loading';
 
 class Album extends Component {
-  render() {
-    return (
-      <>
-        <Header />
-        <div data-testid="page-album" />
+  constructor() {
+    super();
+    this.state = {
+      music: [],
+      dataInfoAlbum: [],
+      isLoading: false,
 
-      </>
+    };
+  }
+
+  componentDidMount() {
+    this.getMusicsById();
+  }
+
+  getMusicsById = async () => {
+    const { match } = this.props;
+    const { id } = match.params;
+    this.setState({ isLoading: true });
+    const musicsResponse = await getMusics(id);
+    const onlyRealTracks = musicsResponse.filter((ElementTracks) => ElementTracks.kind
+    === 'song');
+    console.log(onlyRealTracks);
+
+    this.setState({ music: onlyRealTracks,
+      dataInfoAlbum: musicsResponse[0],
+      isLoading: false });
+  };
+
+  render() {
+    const { music, isLoading, dataInfoAlbum } = this.state;
+    return (
+      <div data-testid="page-album">
+
+        <Header />
+
+        {isLoading && <Loading />}
+        {
+          isLoading || (
+
+            <>
+              <div data-testid="artist-name">{dataInfoAlbum.artistName}</div>
+
+              <div data-testid="album-name">{dataInfoAlbum.collectionName}</div>
+              <div className="songsList">
+                {music.map((elementTracks, index) => (
+
+                  <MusicCard
+                    trackName={ elementTracks.trackName }
+                    key={ index }
+                    previewUrl={ elementTracks.previewUrl }
+                  />
+                ))}
+              </div>
+            </>
+
+          )
+        }
+
+      </div>
 
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string }),
+  }).isRequired,
+};
 
 export default Album;
---
+++ src/pages/compoents/MusicCard.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import * as PropTypes from 'prop-types';
+
+class MusicCard extends Component {
+  render() {
+    const { trackName, previewUrl } = this.props;
+    return (
+      <div>
+        <h3>{trackName}</h3>
+        <audio data-testid="audio-component" src={ previewUrl } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          {' '}
+          {' '}
+          <code>audio</code>
+          .
+        </audio>
+      </div>
+    );
+  }
+}
+MusicCard.propTypes = {
+  trackName: PropTypes.string.isRequired,
+  previewUrl: PropTypes.string.isRequired,
+
+};
+export default MusicCard;
